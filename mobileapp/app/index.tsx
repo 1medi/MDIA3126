32a05c37370eb78a0a544fd787a17524ff815b47
1medi <144403286+1medi@users.noreply.github.com>
2024-09-11 01:03:10 -0700

    hehe

---
+++ mobileapp/app/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text style={{
+        fontSize:50
+      }}>Banana</Text>
+    </View>
+  );
+}
